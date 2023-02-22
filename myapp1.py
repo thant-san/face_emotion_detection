ff1532d1479a4a6744c5c0f0f4bdca2f78a21ffd
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-23 00:12:06 +0630

    Create myapp1.py

---
+++ myapp1.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image,ImageOps
+import numpy as np
+import tensorflow as tf
+import cv2
+from tensorflow.keras.models import load_model
+model=load_model('ths.h5')
+file_upload=st.file_uploader("insert")
+img=Image.open(file_upload)
+size=(277,277)
+img=ImageOps.fit(img,size,Image.ANTIALIAS)
+st.image(img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
+string=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
+st.write('string')
