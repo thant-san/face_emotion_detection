cdd13daeac947cdd402546b7b1db169e179bbb78
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-24 22:19:44 +0630

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 model=load_model('Fer_Model.h5')
 file_upload=st.file_uploader("insert")
 img=Image.open(file_upload)
-size=(277,277)
+size=(48,48)
 img=ImageOps.fit(img, size, Image.ANTIALIAS)
 st.image(img)
 img=ImageOps.grayscale(img)
@@ -16,5 +16,5 @@ st.image(img)
 img=np.asarray(img)
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
-string=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
+string=np.argmax(model.predict(img, axis=-1)[0]
 st.write('string')
