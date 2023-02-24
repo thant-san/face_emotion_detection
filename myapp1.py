b40676c3f188957b3e19b8accfcbe6706cc90bfd
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-24 22:10:01 +0630

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -9,10 +9,10 @@ model=load_model('Fer_Model.h5')
 file_upload=st.file_uploader("insert")
 img=Image.open(file_upload)
 size=(277,277)
-img=ImageOps.fit(img,size,Image.ANTIALIAS)
+img=ImageOps.grayscale(img)
 st.image(img)
-img=cv2.imread(img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
 string=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
 st.write('string')
