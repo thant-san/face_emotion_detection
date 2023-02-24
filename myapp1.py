ae8a1bcb64436db4eb9a1f708e915f40c5f981b6
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-24 22:29:12 +0630

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -7,15 +7,19 @@ import cv2
 from tensorflow.keras.models import load_model
 model=load_model('Fer_Model.h5')
 file_upload=st.file_uploader("insert")
-img=Image.open(file_upload)
-size=(48,48)
-img=ImageOps.fit(img, size, Image.ANTIALIAS)
-st.image(img)
-img=ImageOps.grayscale(img)
-st.image(img)
-img=np.asarray(img)
-st.write(img.shape)
+if file_upload is None:
+  st.write("input image")
+else:
+  img=Image.open(file_upload)
+
+  size=(48,48)
+  img=ImageOps.fit(img, size, Image.ANTIALIAS)
+  st.image(img)
+  img=ImageOps.grayscale(img)
+  st.image(img)
+  img=np.asarray(img)
+  st.write(img.shape)
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
-string=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
-st.write(string)
+  emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
+  tring=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
+  st.write(string)
