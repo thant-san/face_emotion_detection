7edd81c6533e93817382a0dbcef6d9ea7d5bca21
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-24 23:27:10 +0630

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -5,12 +5,13 @@ import numpy as np
 import tensorflow as tf
 import cv2
 from tensorflow.keras.models import model_from_json
+
 #load_model
-json_file=open('model.json','r')
-loaded_model_json=json_file.read()
+json_file = open("model.json", "r")
+loaded_json_model = json_file.read()
 json_file.close()
-classifier=model_from_json(loaded_model_json)
-classifier.load_weights('Fer_Model.h5')
+model = model_from_json(loaded_json_model)
+model.load_weights("model_weights.h5")
 
 file_upload=st.file_uploader("insert")
 if file_upload is None:
@@ -27,6 +28,6 @@ else:
   st.write(img.shape)
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
-  max_index=np.argmax(classifier.predict(img.reshape((1,48,48,1))), axis=-1)[0]
+  max_index=np.argmax(model.predict(img.reshape((1,48,48,1))), axis=-1)[0]
   predicted_emotion = emotions[max_index] 
   st.write(f'predicted emotion is {predicted_emotion}')
