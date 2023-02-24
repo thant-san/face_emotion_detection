55659110c589084360580462cfcf0355447bf151
thant htoo san <102045904+thant-san@users.noreply.github.com>
2023-02-25 02:09:36 +0630

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -60,14 +60,13 @@ def main():
     if choice == "Home":
         html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
                                             <h4 style="color:white;text-align:center;">
-                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+                                            Face Emotion detection application using OpenCV, trained CNN model and Streamlit.</h4>
                                             </div>
                                             </br>"""
         st.markdown(html_temp_home1, unsafe_allow_html=True)
         st.write("""
-                 The application has two functionalities.
-                 1. Real time face detection using web cam feed.
-                 2. Real time face emotion recognization.
+                 The application will detect face and in the mean time ,will predict user emotion.
+                 
                  """)
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
@@ -79,21 +78,12 @@ def main():
         st.subheader("About this app")
         html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
                                     <h4 style="color:white;text-align:center;">
-                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
+                                    Real time face emotion detection application using OpenCV,  Trained CNN model and Streamlit.</h4>
                                     </div>
                                     </br>"""
         st.markdown(html_temp_about1, unsafe_allow_html=True)
 
-        html_temp4 = """
-                             		<div style="background-color:#98AFC7;padding:10px">
-                             		<h4 style="color:white;text-align:center;">This Application is developed by Mohammad Juned Khan using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at Mohammad.juned.z.khan@gmail.com. </h4>
-                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
-                             		</div>
-                             		<br></br>
-                             		<br></br>"""
-
-        st.markdown(html_temp4, unsafe_allow_html=True)
-
+       
     else:
         pass
 
